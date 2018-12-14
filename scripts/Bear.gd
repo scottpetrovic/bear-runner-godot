6e9f094357a8c8250fe0a51a862e6cc63518ded6
Scott Petrovic <scottpetrovic@gmail.com>
2018-12-13 19:39:55 -0600

    Replace cylinder with a bear and update script so it rotates and follows

--- scripts/Bear.gd
+++ scripts/Bear.gd
@@ -10,23 +10,34 @@ extends KinematicBody
 # Kinematic - possible use for character. Inherits from kinematic body
 
 var playerRef
-var movementSpeed = 0.4 
+var movementSpeed = 2.0 
 var directionToGo = Vector3(0,0,0) # calculated each frame
+var upDirection = Vector3(0, 1, 0 ) 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	playerRef =  get_node("../Player") # maybe a better way to get this??
 
-
 func _physics_process(delta):
+	
 	if playerRef:
-		directionToGo = playerRef.translation - translation
+		directionToGo = playerRef.translation
 		directionToGo.y = 0 # stops Bear from flying to follow Player
 	else:
 		print("player reference not found")
 
-	var linear_velocity = directionToGo * (movementSpeed * delta)	
-	var collisionData = move_and_collide(linear_velocity) # returns KinematicCollision
+	
+	# var lookatDirection = transform.looking_at(directionToGo, upDirection )
+	look_at(directionToGo, upDirection)
+	
+	var objectForwardDirection = -get_transform().basis.z
+	var objectUpDirection = get_transform().basis.y;
+
+	
+	var forward_velocity =  objectForwardDirection *  movementSpeed * delta	
+	var collisionData = move_and_collide(forward_velocity) # returns KinematicCollision
+	
+	
 	
 	if collisionData:
 		if collisionData.collider.name == "Player":
